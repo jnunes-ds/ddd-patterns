6b21350531cb3bf5e1fb31b2e31f8002974f9633
Denison Junior <denison.junior@cebraspe.org.br>
2025-02-20 23:06:16 -0300

    feat: check typescript before execute tests

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ function main () {
   customer.address = new Address("Rua do beco", 123, "Brasília", "DF", "12345-678");
   customer.activate();
 
-  const item1 = new OrderItem("1", "Item 1", 100);
-  const item2 = new OrderItem("2", "Item 2", 15);
+  const item1 = new OrderItem("1", "product1", "Item 1", 100, 1);
+  const item2 = new OrderItem("2", "product245", "Item 2", 15, 3);
 
   const order = new Order("1", "123", [item1, item2]);
   order.printOrder();
