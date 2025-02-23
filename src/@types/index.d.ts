e68b3ddcbdb5b35f6d73c56206ea2bc34b4e36d7
Denison Junior <denison.junior@cebraspe.org.br>
2025-02-23 15:34:17 -0300

    feat: create NonPrimitive type to make more clear declarations

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -5,4 +5,5 @@ declare global {
     Enumerate<N2>, Enumerate<N1>
   >;
 
-type RangeN<N1 extends number, N2 extends number> = Exclude<Enumerate<N2>, Enumerate<N1>>;
+  type NonPrimitive = object;
+}
