1e188866d6d0cc53dfeae327b375897df9866ec0
Denison Junior <denison.junior@cebraspe.org.br>
2025-02-23 15:08:24 -0300

    test: add service to place order adding reward points to customer

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,8 +1,20 @@
 import OrderItem from "../entity/order_item";
 import Order from "../entity/order";
 import OrderService from "./order.service";
+import Customer from "../entity/customer";
 
 describe("Order Service Unit tests", () => {
+
+  it("should place an order", () => {
+    const customer = new Customer("c1", "Customer 1");
+    const item1 = new OrderItem("i1", "Item 1", "Produto 1", 10, 1);
+
+    const order = OrderService.placeOrder(customer, [item1]);
+
+    expect(customer.rewardPoints).toBe(5);
+    expect(order.total()).toBe(10);
+  });
+
   it("should get total of all orders", () => {
     const orderItem1 = new OrderItem("i1", "Item 1", "Produto 1", 100, 1);
     const orderItem2 = new OrderItem("i2", "Item 2", "Produto 2", 200, 2);
