ffb96af3153ac8a6d60500bf992a73560787d066
Denison Junior <denison.junior@cebraspe.org.br>
2025-02-23 15:08:19 -0300

    feat: add service to place order adding reward points to customer

--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -1,6 +1,16 @@
 import Order from "../entity/order";
+import Customer from "../entity/customer";
+import OrderItem from "../entity/order_item";
+import {v4 as uuid} from "uuid";
 
 export default class OrderService {
+  public static placeOrder(customer: Customer, items: OrderItem[]): Order {
+    if (!items.length) throw new Error("Order must have at least one item");
+    const order = new Order(uuid(), customer.id, items);
+    customer.addRewardPoints(order.total() / 2);
+    return order;
+  }
+
   public static total(orders: Order[]): number {
     return orders.reduce((acc, order) => {
       return acc + order.total();
