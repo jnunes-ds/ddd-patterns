8427b6ff7fb0e8fca7cff81ca781040c40809531
Denison Junior <denison.junior@cebraspe.org.br>
2025-02-25 15:43:19 -0300

    test: implement findAll method

--- src/infrastructure/repository/order.repository.spec.ts
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -310,4 +310,81 @@ describe("Order Repository Unit tests", () => {
     await expect(orderRepository.findAllCustomerOrders(customer.id)).rejects.toThrow("These customer have no orders yet.");
   });
 
+  it("should find all orders", async () => {
+    // create customer
+    const customerRepository = new CustomerRepository();
+    const customer1 = new Customer("c1", "Customer 1");
+    const customer2 = new Customer("c2", "Customer 2");
+    const address1 = new Address("Street 1", 12, "City", "State", "12345-123");
+    const address2 = new Address("Street 2", 22, "City", "State", "12345-456");
+    customer1.changeAddress(address1);
+    customer1.activate();
+    customer2.changeAddress(address2);
+    customer2.activate();
+    await customerRepository.create(customer1);
+    await customerRepository.create(customer2);
+
+    // create product
+    const productRepository = new ProductRepository()
+    const product1 = new Product("p1", "Product 1", 10);
+    const product2 = new Product("p2", "Product 2", 20);
+    const product3 = new Product("p3", "Product 3", 30);
+    await productRepository.create(product1);
+    await productRepository.create(product2);
+    await productRepository.create(product3);
+
+    // create order item
+    const orderItem1 = new OrderItem(
+      "i1",
+      product1.id,
+      product1.name,
+      product1.price,
+      2
+    );
+    const orderItem2 = new OrderItem(
+      "i2",
+      product2.id,
+      product2.name,
+      product2.price,
+      2
+    );
+    const orderItem3 = new OrderItem(
+      "i3",
+      product3.id,
+      product3.name,
+      product3.price,
+      2
+    );
+    const orderItem4 = new OrderItem(
+      "i4",
+      product2.id,
+      product2.name,
+      product2.price,
+      1
+    );
+
+    // create order
+    const orderRepository = new OrderRepository();
+    const order1 = new Order("o1", customer1.id, [orderItem1, orderItem2]);
+    const order2 = new Order("o2", customer2.id, [orderItem3, orderItem4]);
+    await orderRepository.create(order1);
+    await orderRepository.create(order2);
+
+    const orders = await orderRepository.findAll();
+
+    expect(orders).toEqual([order1, order2]);
+  });
+
+  it("should throw an error when the system has no orders yet", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("c1", "Customer 1");
+    const address = new Address("Street 1", 12, "City", "State", "12345-123");
+    customer.changeAddress(address);
+    customer.activate();
+    await customerRepository.create(customer);
+
+    const orderRepository = new OrderRepository();
+    await expect(orderRepository.findAll()).rejects.toThrow("There are no orders yet.");
+  });
+
 });
