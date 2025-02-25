43a53d0f27366b6405a3bc857673302ee2727968
Denison Junior <denison.junior@cebraspe.org.br>
2025-02-25 15:39:44 -0300

    feat: create findAllCustomerOrders method

--- src/domain/repository/order-repository.ts
+++ src/domain/repository/order-repository.ts
@@ -4,4 +4,5 @@ import OrderItem from "@domain/entity/order_item";
 
 export default interface IOrderRepository extends RepositoryInterface<Order> {
   findOrderItem(orderItemId: string, orderId: string): Promise<OrderItem>;
+  findAllCustomerOrders(customerId: string): Promise<Order[]>;
 }
