fab1ec17b08d0eba7fb54794d1249089822f66cd
Denison Junior <denison.junior@cebraspe.org.br>
2025-02-25 14:51:15 -0300

    test: implement find order method

--- src/infrastructure/repository/order.repository.spec.ts
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -159,4 +159,38 @@ describe("Order Repository Unit tests", () => {
       ]
     });
   });
+
+  it("should find an order", async () => {
+    // create customer
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("c1", "Customer 1");
+    const address = new Address("Street 1", 12, "City", "State", "12345-123");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    // create product
+    const productRepository = new ProductRepository()
+    const product = new Product("p1", "Product 1", 10);
+    await productRepository.create(product);
+
+    // create order item
+    const orderItem = new OrderItem(
+      "i1",
+      product.id,
+      product.name,
+      product.price,
+      2
+    );
+
+    // create order
+    const orderRepository = new OrderRepository();
+    const order = new Order("o1", customer.id, [orderItem]);
+    await orderRepository.create(order);
+
+    // find order on repository
+    const foundOrder = await orderRepository.find(order.id);
+
+    // check if order was found
+    expect(foundOrder).toEqual(order);
+  });
 });
