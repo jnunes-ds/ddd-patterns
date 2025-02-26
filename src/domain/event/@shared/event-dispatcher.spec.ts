60415a84f0f54d5ac45789c2c12fdad74245012b
Denison Junior <denison.junior@cebraspe.org.br>
2025-02-25 22:22:52 -0300

    test: add unregister method

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -12,4 +12,17 @@ describe("Domain events tests", () => {
     expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
     expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
   });
+
+  it("should unregister an event handler", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+
+    eventDispatcher.unregister("ProductCreatedEvent", eventHandler);
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(0);
+  });
+
 });
