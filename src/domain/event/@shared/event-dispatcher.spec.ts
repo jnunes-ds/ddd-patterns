bfc0ed96d5dd20d8b0a93be36baaf418c998a8a4
Denison Junior <denison.junior@cebraspe.org.br>
2025-02-25 22:11:29 -0300

    test: create product event, dispatcher and handler to send email when a new product is created

---
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -0,0 +1,15 @@
+import EventDispatcher from "./event-dispatcher";
+import SendEmailWhenProductIsCreatedHandler from "@domain/event/product/handler/send-email-when-product-is-created.handler";
+
+describe("Domain events tests", () => {
+  it("should register an evento handler", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+  });
+});
