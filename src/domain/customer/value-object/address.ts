2d1344998e4772b3fc750348e565e3762dddf8c4
Denison Junior <denison.junior@cebraspe.org.br>
2025-03-02 19:46:55 -0300

    refator: use simple way to declare Address class

--- src/domain/customer/value-object/address.ts
+++ src/domain/customer/value-object/address.ts
@@ -1,23 +1,11 @@
 export default class Address {
-
-    private _street: string;
-    private _number: number;
-    private _city: string;
-    private _state: string;
-    private _zipCode: string;
-
   constructor(
-    street: string,
-    number: number,
-    city: string,
-    state: string,
-    zipCode: string
+    private _street: string,
+    private _number: number,
+    private _city: string,
+    private _state: string,
+    private _zipCode: string,
   ) {
-    this._street = street;
-    this._number = number;
-    this._city = city;
-    this._state = state;
-    this._zipCode = zipCode;
     this.validate();
   }
 
