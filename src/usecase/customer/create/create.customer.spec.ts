02681dace19b1a8826844b9cac554c1d49b05b83
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-03 19:55:11 -0300

    test: check if it throw an error when street is missing

--- src/usecase/customer/create/create.customer.spec.ts
+++ src/usecase/customer/create/create.customer.spec.ts
@@ -43,4 +43,15 @@ describe("Unit Test - Create Customer use Case", () => {
 
     await expect(output).rejects.toThrowError("Name is required");
   });
+
+  it("should throw an error when street is missing", async () => {
+    const customerRepository = MockRepository();
+    const usecase = new CreateCustomerUseCase(customerRepository);
+
+    input.address.street = "";
+
+    const output = usecase.execute(input);
+
+    await expect(output).rejects.toThrowError("Street is required");
+  });
 });
