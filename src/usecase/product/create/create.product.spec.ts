11db05af2f401cb0baf038f98313851345f933c6
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-05 13:15:47 -0300

    test: create product use case

---
+++ src/usecase/product/create/create.product.spec.ts
@@ -0,0 +1,58 @@
+import {InputCreateProductDTO} from "./create.product.dto";
+import CreateProductUsecase from "./create.product.usecase";
+
+const input: InputCreateProductDTO = {
+  name: "Batata frita",
+  price: 10,
+};
+
+const MockRepository = () => ({
+  find: jest.fn(),
+  findAll: jest.fn(),
+  create: jest.fn(),
+  update: jest.fn(),
+});
+
+describe("Unit Test - Create Product Use Case", () => {
+  it("should create a product", async () => {
+    const productRepository = MockRepository();
+    const usecase = new CreateProductUsecase(productRepository);
+
+    const output = await usecase.execute(input);
+
+    expect(productRepository.create).toHaveBeenCalled();
+    expect(output).toEqual({
+      id: expect.any(String),
+      name: input.name,
+      price: input.price,
+    });
+  });
+
+  it("should throw an error when name is missing", async () => {
+    const productRepository = MockRepository();
+    const usecase = new CreateProductUsecase(productRepository);
+
+    const inputWithoutName: InputCreateProductDTO = {
+      ...input,
+      name: "",
+    }
+
+    const output = usecase.execute(inputWithoutName);
+
+    await expect(output).rejects.toThrowError("Name is required");
+  });
+
+  it("should throw an error when price is missing", async () => {
+    const productRepository = MockRepository();
+    const usecase = new CreateProductUsecase(productRepository);
+
+    const inputWithoutPrice: InputCreateProductDTO = {
+      ...input,
+      price: 0,
+    }
+
+    const output = usecase.execute(inputWithoutPrice);
+
+    await expect(output).rejects.toThrowError("Price must be grater than 0");
+  });
+});
---
+++ src/usecase/product/create/create.product.test.ts
@@ -0,0 +1,57 @@
+import {InputCreateProductDTO} from "./create.product.dto";
+import CreateProductUsecase from "./create.product.usecase";
+import {Sequelize} from "sequelize-typescript";
+import ProductModel from "@infra/product/repository/sequelize/product.model";
+import ProductRepository from "@infra/product/repository/sequelize/product.repository";
+
+const input: InputCreateProductDTO = {
+  name: "Batata frita",
+  price: 10,
+};
+
+
+describe("Integration Test - Create Product Use Case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+
+  test("create a product usecase and it's integration with product repository", async () => {
+    const productRepository = new ProductRepository();
+    const usecase = new CreateProductUsecase(productRepository);
+
+    const output = await usecase.execute(input);
+
+    const iputWithoutName = {
+      ...input,
+      name: "",
+    }
+
+    const inputWithoutPrice = {
+      ...input,
+      price: 0,
+    }
+
+    expect(output).toEqual({
+      id: expect.any(String),
+      name: input.name,
+      price: input.price,
+    });
+    await expect(usecase.execute(iputWithoutName)).rejects.toThrowError("Name is required");
+    await expect(usecase.execute(inputWithoutPrice)).rejects.toThrowError("Price must be grater than 0");
+  });
+});
