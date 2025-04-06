31ac0b5f7a6a045b333a2f2c17e20f6eda961277
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-06 14:54:15 -0300

    test: add list method to customer route

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -47,4 +47,63 @@ describe('E2E Test - Customer', () => {
 
     expect(response.status).toBe(500);
   });
+
+  it("should list all customers", async () => {
+    const response = await request(app)
+      .post('/api/customers')
+      .send({
+        name: 'John Doe',
+        address: {
+          street: '123 Main St',
+          number: 156,
+          city: 'Anytown',
+          state: 'CA',
+          zipCode: '12345'
+        }
+      });
+    expect(response.status).toBe(200);
+
+    const response2 = await request(app)
+      .post('/api/customers')
+      .send({
+        name: 'Jane Doe',
+        address: {
+          street: '456 Elm St',
+          number: 789,
+          city: 'Othertown',
+          state: 'NY',
+          zipCode: '67890'
+        }
+      });
+    expect(response2.status).toBe(200);
+
+    const listResponse = await request(app).get('/api/customers').send();
+    expect(listResponse.status).toBe(200);
+    expect(listResponse.body).toEqual({
+      customers: [
+        {
+          id: expect.any(String),
+          name: 'John Doe',
+          address: {
+            street: '123 Main St',
+            number: 156,
+            city: 'Anytown',
+            state: 'CA',
+            zipCode: '12345'
+          }
+        },
+        {
+          id: expect.any(String),
+          name: 'Jane Doe',
+          address: {
+            street: '456 Elm St',
+            number: 789,
+            city: 'Othertown',
+            state: 'NY',
+            zipCode: '67890'
+          }
+        }
+      ]
+    });
+  });
 });
