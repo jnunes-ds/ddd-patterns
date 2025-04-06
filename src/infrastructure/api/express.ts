f2a8af91c0169b04177df8744e010db8239e9088
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-06 12:32:29 -0300

    feat: create express server

---
+++ src/infrastructure/api/express.ts
@@ -0,0 +1,21 @@
+import 'module-alias/register';
+import express, {type Express} from "express";
+import {Sequelize} from "sequelize-typescript";
+import CustomerModel from "@infra/customer/repository/sequelize/customer.model";
+
+export const app: Express = express();
+app.use(express.json());
+
+export let sequelize: Sequelize;
+
+async function setupDb() {
+  sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: ":memory:",
+    logging: false,
+  })
+  await sequelize.addModels([CustomerModel]);
+  await sequelize.sync();
+}
+
+setupDb();
