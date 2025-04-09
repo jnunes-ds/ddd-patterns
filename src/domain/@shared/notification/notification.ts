9e972a893ec16d8afb58315cef4aa2302b2d27fc
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-08 21:04:36 -0300

    feat: create notification to accumulate errors

--- src/domain/@shared/notification/notification.ts
+++ src/domain/@shared/notification/notification.ts
@@ -1,12 +1,16 @@
-export type NotificationError = {
+export type NotificationErrorProps = {
   message: string;
   context: string;
 }
 
 export default class Notification {
-  private errors: NotificationError[] = [];
+  private errors: NotificationErrorProps[] = [];
 
-  addError(error: NotificationError) {
+  getErrors(): NotificationErrorProps[] {
+    return this.errors;
+  }
+
+  addError(error: NotificationErrorProps) {
     this.errors.push(error);
   }
 
