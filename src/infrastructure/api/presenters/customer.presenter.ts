c5a3d36c04baf7ecf5f71b9396c339146262621a
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-10 18:38:13 -0300

    feat: create customer presenter with to xml method

---
+++ src/infrastructure/api/presenters/customer.presenter.ts
@@ -0,0 +1,28 @@
+import {toXML, XmlOptions} from 'jstoxml';
+import {OutputListCustomerDTO} from "@usecase/customer/list/list.customer.dto";
+
+export default class CustomerPresenter {
+  static toXML(data: OutputListCustomerDTO): string {
+    const xmlOptions: XmlOptions = {
+      header: true,
+      indent: '  ',
+    }
+
+    return toXML({
+      customers: {
+        customer: data.customers.map(customer => ({
+          id: customer.id,
+          name: customer.name,
+          address: {
+            street: customer.address.street,
+            number: customer.address.number,
+            city: customer.address.city,
+            state: customer.address.state,
+            zipCode: customer.address.zipCode
+          }
+        }))
+      }
+    }, xmlOptions);
+  }
+
+}
