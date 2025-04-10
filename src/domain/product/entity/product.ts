80685b210c8c4bb9e6ecca0a393e33a94863d3a8
Denison Junior <denison.junior@cebraspe.org.br>
2025-04-10 12:27:48 -0300

    feat: use notification to throw all errors at once

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,16 +1,19 @@
 import IProduct from "./product.interface";
+import Entity from "@domain/@shared/entity/entity.abstract";
 
-export default class Product implements IProduct {
+export default class Product  extends Entity implements IProduct {
   constructor(
-    private _id: string,
+    id: string,
     private _name: string,
     private _price: number
   ) {
+    super();
+    this._id = id;
     this.validate();
-  }
 
-  get id(): string {
-    return this._id;
+    if(this.notification.hasErrors()) {
+      throw new Error(this.notification.getErrors().map((error) => `${error.context}: ${error.message}`).join(", ").replace(/, $/, ''));
+    }
   }
 
   get name() {
@@ -22,9 +25,18 @@ export default class Product implements IProduct {
   }
 
   validate() {
-    if (!this._id) throw new Error("Id is required");
-    if (!this._name) throw new Error("Name is required");
-    if (!this._price || this._price < 0) throw new Error("Price must be grater than 0");
+    if (!this._id) this.notification.addError({
+      context: "product",
+      message: "Id is required",
+    });
+    if (!this._name) this.notification.addError({
+      context: "product",
+      message: "Name is required",
+    });
+    if (!this._price || this._price < 0) this.notification.addError({
+      context: "product",
+      message: "Price must be greater than 0",
+    });
   }
 
   changeName(name: string) {
